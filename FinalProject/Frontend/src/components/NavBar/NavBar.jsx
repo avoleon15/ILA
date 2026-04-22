f56da3d86224445b006386222a9251a8e29b2bf1
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-04-22 08:42:51 -0600

    nav bar

---
+++ FinalProject/Frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+
+import './NavBar.css';
+
+function NavBar() {
+
+  return (
+
+    <div className="container">
+        <h3>hola</h3>
+        <h3>hola</h3>
+        <h3>hola</h3>
+    </div>
+
+
+
+  );
+}
+
+export default NavBar
