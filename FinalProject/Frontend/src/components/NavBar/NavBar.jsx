4a81429bf89b5a55197720e22a2473270574e9c6
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-04-23 23:11:33 -0600

    NavBar: img deleted, pixel art finished

--- FinalProject/Frontend/src/components/NavBar/NavBar.jsx
+++ FinalProject/Frontend/src/components/NavBar/NavBar.jsx
@@ -1,18 +1,12 @@
 
 import './NavBar.css';
-import logo from '../../Assets/images/ChatGPT Image 22 abr 2026, 10_49_12.png';
+
 
 function NavBar() {
 
   return (
 
     <div className="NavBar">
-      <div className='Left'>
-      <img src={logo} alt='Knight logo'/> 
-      </div>
-      <div className='center'>
-      <h3> </h3>
-      </div>
       <div className='pixel'>
       </div> 
     </div>
