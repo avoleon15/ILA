e382b91ca91358f5be8f0b40502abc0703f75873
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-04-28 22:52:58 -0600

    join game titleHolder added and h2

--- FinalProject/Frontend/src/screens/JoinGame/JoinGame.jsx
+++ FinalProject/Frontend/src/screens/JoinGame/JoinGame.jsx
@@ -9,6 +9,7 @@ const JoinGame = ({navigate}) => {
   return (
     <div id='Joingame'>
         <TitleHolder text="Join Game"/> 
+        <h2>Enter the 6-digit code to join a game</h2>
     </div>
   )
 }
