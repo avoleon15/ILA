4d29801ecc366fcbbf63301ec60331f7cb165e05
avoleon15 <avoleon15@gmail.com>
2026-04-29 07:45:30 -0600

    hotfixed the createGame, uneceesary changes where added

--- FinalProject/Frontend/src/screens/createGame/createGame.jsx
+++ FinalProject/Frontend/src/screens/createGame/createGame.jsx
@@ -1,23 +1,16 @@
 import { useState } from "react"
+import Gamemode from "../../components/Gamemode/Gamemode.jsx"
+import OptionHolder from "../../components/OptionHolder/OptionHolder.jsx"
 import NumberInput from '../../components/PlayersNum/PlayersNum.jsx'
 import TitleHolder from '../../components/TitleHolder/TitleHolder.jsx'
 import './createGame.css'
-import { useState } from "react"; 
-import Gamemode from "../../components/Gamemode/Gamemode.jsx"
-import OptionHolder from "../../components/OptionHolder/OptionHolder.jsx"
-import BackButton from '../../components/BackButton/BackButton.jsx'
 
 function CreateGame({ navigate }) {
     const [players, setPlayers] = useState(2)
 
     return (
         <section id='createGame'>
-            <TitleHolder text="Create game"/>
             
-            <input type="number" 
-            value={valor} 
-            onChange={(e) => setValor(e.target.value)} 
-            placeholder="número de jugadores" /> 
             <TitleHolder text="Create Game" />
             <NumberInput
                 label="NUM OF PLAYERS"
