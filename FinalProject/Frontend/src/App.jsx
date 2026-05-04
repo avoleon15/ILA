49937fff9a26dac59ab0396e44c916b2a1f8b8d0
avoleon15 <avoleon15@gmail.com>
2026-05-04 09:31:17 -0600

    Changed the set menu on App

--- FinalProject/Frontend/src/App.jsx
+++ FinalProject/Frontend/src/App.jsx
@@ -8,7 +8,7 @@ import GameRouter from './GameRouter.jsx'
 
 function App() {
 
-  const [page, setPage] = useState('winner')
+  const [page, setPage] = useState('menu')
 
   const [gameState, setGameState] = useState({
     playerName: '',
