073429c793aa5a9e9ff68d48d57b118e18b8b500
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-05-04 10:31:52 -0600

    a

--- FinalProject/Frontend/src/App.jsx
+++ FinalProject/Frontend/src/App.jsx
@@ -8,7 +8,7 @@ import GameRouter from './GameRouter.jsx'
 
 function App() {
 
-  const [page, setPage] = useState('selecTopic')
+  const [page, setPage] = useState('winner')
 
   const [gameState, setGameState] = useState({
     playerName: '',
