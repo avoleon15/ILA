865f22601dfb047fbce04469e79c009e2989172f
Luis Alejandro Montenegro Najera <luigiale14@gmail.com>
2026-05-05 21:38:53 -0600

    hotfix: went back to menu route

--- FinalProject/Frontend/src/App.jsx
+++ FinalProject/Frontend/src/App.jsx
@@ -8,7 +8,7 @@ import GameRouter from './GameRouter.jsx'
 
 function App() {
 
-  const [page, setPage] = useState('voting')
+  const [page, setPage] = useState('menu')
 
   const [gameState, setGameState] = useState({
     playerName: '',
